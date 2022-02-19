435c851b0b67a549b63c6484d3333d2e4d5518c3
Mr-SimpleEditor <89197795+Mr-SimpleEditor@users.noreply.github.com>
2022-02-19 12:16:19 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+
+scriptcs is for running c# program in Linux
+because it is not avaiable for linux i have created this for Linux users
+
+In Linux mono is used to run the C# program, but everytime we run we have to create it's .exe file and then execute it.
+So to save this time i've created this scriptcs which contains the mono commands so it is automated program
+
+ 
+Steps to install :
+
+This command will install important packages and setup your scriptcs
+ 
+1. sudo bash install.sh
+
+Now you are ready to rock!
+
+
+syntax : 
+
+ scriptcs <File.cs>
+
+
+
+If caught any error please let me know.
+
---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+R='\033[31m'  # red
+G='\033[32m'  # green
+C='\033[36m'  # cyan
+W='\033[0m'   # white
+Y='\033[33m'  # yellow
+
+
+
+if [ -f '/etc/arch-release' ]; then
+	sudo pacman -S espeak-ng >/dev/null
+	espeak-ng "Installation is in Progress " 1>/dev/null
+	sudo pacman -S mono-devel
+	sudo pacman -S aspnet-runtime 
+	sudo pacman -S dotnet-sdk
+	sudo chmod +x scriptcs
+	sudo mv scriptcs /bin/
+if [ -f '/etc/fedora-release' ]; then
+	sudo dnf install mono-devel
+	sudo dnf install nunit nunit-gui
+	sudo chmod +x scriptcs
+        sudo mv scriptcs /bin/
+if [ -f '/etc/debian_version' ]; then
+	sudo apt install gnupg ca-certificates
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+	echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+	sudo apt update
+	sudo apt install mono-devel
+	sudo chmod +x scriptcs
+        sudo mv scriptcs /bin/
+if [ -f '/etc/kali-release' ]; then
+	sudo apt-get install mono-complete
+	sudo apt install mono-devel
+	sudo chmod +x scriptcs
+        sudo mv scriptcs /bin/
+if [ $OSTYPE == 'linux-android' ]; then
+	apt update 
+	apt upgrade -y	
+	apt install mono
+	chmod +x scriptcs
+        mv scriptcs $PREFIX/bin/
+else
+	printf " ${R} Sorry couldn't find distro"
+	exit
+fi
+
+
+echo -e " ${G} Thanks for using"
+
+echo " ${Y} NOTE : Now your mono is installed which is automated for exectution of C#, script named as scriptcs. Now you can execute you file from anywhere, You can use vscode also without any problem"
+
